7a8e9bfc4d66623cc3f52186bb9ff248e8429af8
xijiaxiang <2211508055@ujs.edu.cn>
2017-09-05 15:13:58 +0800

    28题，求全排列

---
+++ src/sword/chapter02/_28permutation/Solution.java
@@ -0,0 +1,29 @@
+package sword.chapter02._28permutation;
+
+public class Solution {
+	public static void main(String[] args) {
+		String[] test={"a","b","c"};
+		permutation(test, 0, test.length);
+	}
+	public static void permutation(String[] str,int start,int len){
+		if(start==len-1){
+			for(int i=0;i<len;i++){
+				System.out.println(str[i]+" ");
+			}
+			System.out.println();
+		}else{
+			for(int i=start;i<len;i++){
+				swap(str,start,i);//交换start和i位
+				permutation(str, start+1, len);
+				swap(str,start,i);
+			}
+		}
+	}
+	public static void swap(String[] str, int i, int j)  
+    {  
+        String temp = new String();  
+        temp = str[i];  
+        str[i] = str[j];  
+        str[j] = temp;  
+    } 
+}
