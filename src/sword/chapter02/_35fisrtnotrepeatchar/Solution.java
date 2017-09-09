cc1c2eec779f6361031c1bbd27131cd630020657
xijiaxiang <2211508055@ujs.edu.cn>
2017-09-09 15:05:48 +0800

    35题，找到只出现一次的字符

---
+++ src/sword/chapter02/_35fisrtnotrepeatchar/Solution.java
@@ -0,0 +1,46 @@
+package sword.chapter02._35fisrtnotrepeatchar;
+
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+
+
+public class Solution {
+	public static void main(String[] args) {
+		String test="aggaccddeeff";
+		char[] array=test.toCharArray();
+		try {
+			System.out.println(FirstNotRepeatChar(array));
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+	}
+	public static char FirstNotRepeatChar(char[] array) throws Exception{
+		Map<Character,Integer> map=new LinkedHashMap<>();
+		for(int i=0;i<array.length;i++){
+			if(map.containsKey(array[i])){
+				map.put(array[i], map.get(array[i])+1);
+			}
+			else{
+				map.put(array[i], 1);
+			}
+		}	
+		
+		boolean isFindfirst=false;
+		char result = 0;
+		for(Entry<Character, Integer> entry:map.entrySet()){
+			if(entry.getValue()==1){
+				isFindfirst=true;
+				result=entry.getKey();
+				break;
+			}
+		}
+		if(isFindfirst){
+			return result;
+		}else{
+			throw new Exception("not found");
+		}
+		
+	}
+}
